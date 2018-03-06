ba76fe43449a25cd49fac66d5b27511b1aac7f62
Julian Andrade <julian@ndrade.com.br>
2018-03-06 16:42:44 -0300

    Initial commit. Authentication info is secure in ignored file whatconverts-auth.php. WhatconvertsAPI class is creating the cURL handler, loading all leads by a start date and buiding a multidimensional array of leads by landing page, separating by lead type.

---
+++ WhatconvertsAPI.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Description of WhatconvertsAPI
+ *
+ * @author Julian Andrade <julian@ndrade.com.br>
+ */
+class WhatconvertsAPI {
+	private $_api_token = null;
+	private $_api_secret = null;
+	private $_curl = null;
+	
+	public function __construct() {
+		require_once 'whatconverts-auth.php';
+		$this->_api_token = $_api_token;
+		$this->_api_secret = $_api_secret;
+		
+		$this->buildCurl();
+	}
+	private function buildCurl() {
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($curl, CURLOPT_USERPWD, $this->_api_token . ":" . $this->_api_secret);
+		
+		$this->_curl = $curl;
+	}
+	
+	public function getLeadByLandingPage($startdate) {
+		$params = [
+			'leads_per_page' => 50,
+			'page_number' => 1,
+			'start_date' => $startdate
+		];
+		
+		$leads_raw = $this->getLeads($params);
+		$leads_by_lp = [];
+		
+		foreach($leads_raw as $lead_item) {
+			$landing_url = $this->parseLandingPageURL($lead_item->landing_url);
+			$lead_type = $lead_item->lead_type;
+			
+			if(!array_key_exists($landing_url, $leads_by_lp)) {
+				$leads_by_lp[$landing_url] = [];
+			}
+			
+			if(!array_key_exists($lead_type, $leads_by_lp[$landing_url])) {
+				$leads_by_lp[$landing_url][$lead_type] = 0;
+			}
+			
+			$leads_by_lp[$landing_url][$lead_type]++;
+		}
+		
+
+		return $leads_by_lp;
+	}
+	private function getLeads($params) {
+		$leads = [];
+		
+		//check if page_number isset. If not, start it with 1
+
+		do {
+			$query = http_build_query($params);
+			curl_setopt($this->_curl, CURLOPT_URL, "https://app.whatconverts.com/api/v1/leads?$query");
+			$response = json_decode(curl_exec($this->_curl));
+			
+			if(property_exists($response, 'error_message')) {
+				break;
+			}
+
+			$leads = array_merge($leads, $response->leads);
+
+			$params['page_number']++;
+			
+		} while( $response->page_number <= $response->total_pages );
+		
+		return $leads;
+	}
+	private function parseLandingPageURL($url) {
+		$parsed = parse_url($url);
+		return $parsed['scheme'] . "://" . $parsed['host'] . $parsed['path'];
+	}
+	
+	function __destruct() {
+		curl_close($this->_curl);
+	}
+}
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php require_once './WhatconvertsAPI.php' ?>
+<?php
+$wcapi = new WhatconvertsAPI();
+$leads = $wcapi->getLeadByLandingPage(date('Y-m-d', strtotime('-60 days')));
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Whatconverts Leads by Landing Page</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<!-- Bootstrap -->
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	</head>
+	<body>
+		<div>
+			<pre><?php var_dump($leads) ?></pre>
+		</div>
+	</body>
+</html>
